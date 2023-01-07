04fcdbf23313aabeccd2a0e998f816e5d7432e0c
Michael Englehorn <michael@englehorn.com>
2023-01-07 13:37:40 -0600

    It's working!

--- homeassistant-mode.el
+++ homeassistant-mode.el
@@ -3,14 +3,14 @@
   :group 'local)
 
 (defcustom mje/homeassistant-url nil
-           "The URL to Home Assistant"
-           :group 'homeassistant-helm
-           :type 'string)
+  "The URL to Home Assistant"
+  :group 'homeassistant-helm
+  :type 'string)
 
 (defcustom mje/homeassistant-api-token nil
-           "Home Assistant API Bearer Token"
-           :group 'homeassistant-helm
-           :type 'string)
+  "Home Assistant API Bearer Token"
+  :group 'homeassistant-helm
+  :type 'string)
 
 (defvar mje/sample-results)
 
@@ -20,52 +20,72 @@
 
 (defun mje/helm-homeassistant-get-states ()
   (request
-   (format "%s/api/states" mje/homeassistant-url)
-   :parser 'json-read
-   :success (cl-function
-	     (lambda (&key data &allow-other-keys)
-	       (mje/helm-homeassistant-format-data data)))
-   :headers '(("Content-Type" . "application/json")("Authorization" . (format "Bearer %s" mje/homeassistant-api-token)))))
+    (format "%s/api/states" mje/homeassistant-url)
+    :parser 'json-read
+    :success (cl-function
+	      (lambda (&key data &allow-other-keys)
+		(mje/helm-homeassistant-format-data data)))
+    :headers (list (cons "Content-Type" "application/json")(cons "Authorization" (format "Bearer %s" mje/homeassistant-api-token)))))
 
 (defun mje/helm-homeassistant-turn-on (device)
   (cond
    ((string-match "^switch\." (cdr (assoc 'entity_id device)))
     (request
-     (format "%s/api/services/switch/turn_on" mje/homeassistant-url)
-     :type "POST"
-     :data (json-encode `(("entity_id" . ,(cdr (assoc 'entity_id device)))))
-     :error
-     (cl-function (lambda (&key error-thrown &allow-other-keys)
-		  (message "Got error: %S" error-thrown)))
-     :headers '(("Content-Type" . "application/json")("Authorization" . (format "Bearer %s" mje/homeassistant-api-token)))))
+      (format "%s/api/services/switch/turn_on" mje/homeassistant-url)
+      :type "POST"
+      :data (json-encode `(("entity_id" . ,(cdr (assoc 'entity_id device)))))
+      :error
+      (cl-function (lambda (&key error-thrown &allow-other-keys)
+		     (message (format "Got error: %S" error-thrown))))
+      :headers (list (cons "Content-Type" "application/json")(cons "Authorization" (format "Bearer %s" mje/homeassistant-api-token)))))
    ((string-match "^light\." (cdr (assoc 'entity_id device)))
     (request
-     (format "%s/api/services/light/turn_on" mje/homeassistant-url)
-     :type "POST"
-     :data (json-encode `(("entity_id" . ,(cdr (assoc 'entity_id device)))))
-     :error
-     (cl-function (lambda (&key error-thrown &allow-other-keys)
-		  (message "Got error: %S" error-thrown)))
-     :headers '(("Content-Type" . "application/json")("Authorization" . (format "Bearer %s" mje/homeassistant-api-token)))))
+      (format "%s/api/services/light/turn_on" mje/homeassistant-url)
+      :type "POST"
+      :data (json-encode `(("entity_id" . ,(cdr (assoc 'entity_id device)))))
+      :error
+      (cl-function (lambda (&key error-thrown &allow-other-keys)
+		     (message (format "Got error: %S" error-thrown))))
+      :headers (list (cons "Content-Type" "application/json")(cons "Authorization" (format "Bearer %s" mje/homeassistant-api-token)))))
    ((string-match "^group\." (cdr (assoc 'entity_id device)))
     (request
-     (format "%s/api/services/light/turn_on" mje/homeassistant-url)
-     :type "POST"
-     :data (json-encode `(("entity_id" . ,(cdr (assoc 'entity_id device)))))
-     :error
-     (cl-function (lambda (&key error-thrown &allow-other-keys)
-		  (message "Got error: %S" error-thrown)))
-     :headers '(("Content-Type" . "application/json")("Authorization" . (format "Bearer %s" mje/homeassistant-api-token)))))))
+      (format "%s/api/services/light/turn_on" mje/homeassistant-url)
+      :type "POST"
+      :data (json-encode `(("entity_id" . ,(cdr (assoc 'entity_id device)))))
+      :error
+      (cl-function (lambda (&key error-thrown &allow-other-keys)
+		     (message (format "Got error: %S" error-thrown))))
+      :headers (list (cons "Content-Type" "application/json")(cons "Authorization" (format "Bearer %s" mje/homeassistant-api-token)))))))
 
 (defun mje/helm-homeassistant-turn-off (device)
-  (request
-   (format "%s/api/services/light/turn_off" mje/homeassistant-url)
-   :type "POST"
-   :data (json-encode `(("entity_id" . ,(cdr (assoc 'entity_id device)))))
-   :error
-   (cl-function (lambda (&key error-thrown &allow-other-keys)
-		(message "Got error: %S" error-thrown)))
-   :headers '(("Content-Type" . "application/json")("Authorization" . (format "Bearer %s" mje/homeassistant-api-token)))))
+  (cond
+   ((string-match "^switch\." (cdr (assoc 'entity_id device)))
+    (request
+      (format "%s/api/services/switch/turn_off" mje/homeassistant-url)
+      :type "POST"
+      :data (json-encode `(("entity_id" . ,(cdr (assoc 'entity_id device)))))
+      :error
+      (cl-function (lambda (&key error-thrown &allow-other-keys)
+		     (message (format "Got error: %S" error-thrown))))
+      :headers (list (cons "Content-Type" "application/json")(cons "Authorization" (format "Bearer %s" mje/homeassistant-api-token)))))
+   ((string-match "^light\." (cdr (assoc 'entity_id device)))
+    (request
+      (format "%s/api/services/light/turn_off" mje/homeassistant-url)
+      :type "POST"
+      :data (json-encode `(("entity_id" . ,(cdr (assoc 'entity_id device)))))
+      :error
+      (cl-function (lambda (&key error-thrown &allow-other-keys)
+		     (message (format "Got error: %S" error-thrown))))
+      :headers (list (cons "Content-Type" "application/json")(cons "Authorization" (format "Bearer %s" mje/homeassistant-api-token)))))
+   ((string-match "^group\." (cdr (assoc 'entity_id device)))
+    (request
+      (format "%s/api/services/light/turn_off" mje/homeassistant-url)
+      :type "POST"
+      :data (json-encode `(("entity_id" . ,(cdr (assoc 'entity_id device)))))
+      :error
+      (cl-function (lambda (&key error-thrown &allow-other-keys)
+		     (message (format "Got error: %S" error-thrown))))
+      :headers (list (cons "Content-Type" "application/json")(cons "Authorization" (format "Bearer %s" mje/homeassistant-api-token)))))))
 
 (defun mje/helm-format-homeassistant-for-display (device)
   (cdr (assoc 'friendly_name (assoc 'attributes device))))
@@ -82,7 +102,7 @@
 
 (defvar mje/helm-source-homeassistant
   '((name . "Home Assistant")
-    (candidates-process . mje/helm-homeassistant-search)
+    (candidates . mje/helm-homeassistant-search)
     (action-transformer . mje/helm-homeassistant-actions)))
 
 (defun helm-homeassistant ()
@@ -91,4 +111,3 @@
   (helm :sources '(mje/helm-source-homeassistant)))
 
 (provide 'helm-homeassistant)
-
